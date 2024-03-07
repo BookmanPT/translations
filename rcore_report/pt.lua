f75a0cc76a1bad9738d2b7c4b99c3009151850ba
BookmanPT <64754185+BookmanPT@users.noreply.github.com>
2024-03-07 14:55:26 +0000

    add pt-PT to locales
    Added Portuguese translation from Portugal to the Rcore_report repository

---
+++ rcore_report/pt.lua
@@ -0,0 +1,61 @@
+Locales['pt'] = {
+    ['admin_on_duty'] = 'Estás de serviço',
+    ['admin_off_duty'] = 'Saiste de serviço',
+    ['saved_all_reports'] = 'Todos os reports foram salvos!',
+    ['saved_admin_chat'] = 'Chat de STAFF salvo!',
+
+    ['report_created'] = 'Report ID %s foi criado com sucesso', -- reportId
+    ['player_added_to_report'] = 'Jogador %s adicionou ao report ID %s', -- playerName, reportId
+    ['report_in_resolve'] = 'Report ID %s está a ser resolvido', -- reportId
+    ['report_closed'] = 'Report ID %s fechado com sucesso', -- reportId
+    ['transcript_report_closed'] = 'Fechar o report',
+    ['new_message_in_report'] = 'Nova mensagem no report ID %s', -- reportId
+    
+    ['kickPlayerFromReport'] = 'Jogador %s foi removido com sucesso do report ID %s', -- playerName, reportId
+    ['playerKickedFromReport'] = 'Foste removido do report ID %s', -- reportId
+
+    -- PLAYER ACTIONS
+    ['teleport_admin_to_player'] = 'Foste teletransportado para o jogador %s', -- playerName
+    ['teleport_player_to_admin'] = 'Jogador %s has foi teletransportado para ti', -- playerName
+    ['freeze_player'] = 'Jogador %s foi congelado', -- playerName
+    ['unfreeze_player'] = 'Jogador %s foi descongelado', -- playerName
+    ['vulnerable_player'] = 'Jogador %s está em modo normal', -- playerName
+    ['invulnerable_player'] = 'Jogador %s está em modo Deus', -- playerName
+    ['heal_player'] = 'Curaste o jogador com sucesso',
+    ['revive_player'] = 'Reviveste o jogador com sucesso',
+    ['pending_reports'] = 'Tens %s reports por resolver',
+
+    ['showPartiallyHiddenNotification'] = 'Pressiona ~%s~ para mostrar o sistema de relatórios',
+
+    ['first_admin_online'] = 'Estão staffs online prontos para atender os teus reports',
+    ['no_admins_online'] = 'Não há staffs online de momento, considera abrir um ticket no discord',
+
+    -- LOGGER
+    ['logger_setTitle'] = '%s Define novo titulo para o report com o ID %s',
+    ['logger_addedMessage'] = '%s mensagem adicionada com o ID %s',
+    ['logger_hideMessage'] = '%s Esconder mensagem com o ID %s',
+    ['logger_showMessage'] = '%s Mostrar mensagem com o ID %s',
+    ['logger_createdReport'] = '%s Criou report com o ID %s',
+    ['logger_adminEnterReport'] = '%s entrou no report com o ID %s',
+    ['logger_adminLeaveReport'] = '%s saiu do report com o ID %s',
+    ['logger_playerKickedFromReport'] = '%s Removido %s do report com o ID %s',
+    ['logger_playerAddedToReport'] = '%s Adicionado %s ao report com o ID %s',
+    ['logger_setInResolve'] = '%s Definido no relatorio do report com o ID %s',
+    ['logger_closedReport'] = '%s Report fechado com o ID %s',
+    ['logger_setTag'] = '%s Definir titulo %s no report com o ID %s',
+    ['logger_clearTag'] = '%s Limpar titulo no report com o ID %s',
+
+    ['CREATE_REPORT'] = 'Criar report',
+    ['SET_IN_RESOLVE'] = 'Definir como resolvido',
+    ['SET_TITLE'] = 'Definir titulo',
+    ['CLOSED_REPORT'] = 'Fechar report',
+    ['ADD_MESSAGE'] = 'Adicionar mensagem',
+    ['HIDE_MESSAGE'] = 'Esconder mensagem',
+    ['SHOW_MESSAGE'] = 'Mostrar mensagem',
+    ['ADD_PLAYER_TO_REPORT'] = 'Adicionar jogador ao report',
+    ['KICK_FROM_REPORT'] = 'Remover jogador do report',
+    ['ADMIN_ENTERS_REPORT'] = 'STAFF entrou no report',
+    ['ADMIN_LEAVES_REPORT'] = 'STAFF saiu do report',
+    ['SET_TAG'] = 'Definir titulo',
+    ['CLEAR_TAG'] = 'Limpar titulo',
+}
